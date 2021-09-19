7fa5fe76c7a0acc044abac1f5950befc2a2b1535
dbnaruto <dbnaruto1@gmail.com>
2021-09-18 23:03:09 -0400

    LeetCode-1 Now with more HashMap!

--- src/main/java/com/dbnaruto/leetcode/n1_TwoSum.java
+++ src/main/java/com/dbnaruto/leetcode/n1_TwoSum.java
@@ -1,5 +1,8 @@
 package main.java.com.dbnaruto.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 1. Two Sum
  * Easy
@@ -11,15 +14,33 @@ package main.java.com.dbnaruto.leetcode;
 public class n1_TwoSum {
     //-------------------------------------------------------
     public static int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++) {
-            if (Math.abs(i) < Math.abs(target) || 0 == target) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (nums[i] + nums[j] == target) {
-                        return new int[]{i, j};
-                    }
-                }
+//        for(int i = 0; i < nums.length; i++) {
+//            if (Math.abs(i) < Math.abs(target) || 0 == target) {
+//                for (int j = i + 1; j < nums.length; j++) {
+//                    if (nums[i] + nums[j] == target) {
+//                        return new int[]{i, j};
+//                    }
+//                }
+//            }
+//        }
+//        return new int[0];
+
+        Map<Integer, Integer> numsMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            numsMap.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int number = nums[i];
+            int remainder = target - number;
+            if (numsMap.containsKey(remainder)) {
+                int remainderIndex = numsMap.get(remainder);
+                if (remainderIndex == i) continue;
+                return new int[]{i, remainderIndex};
             }
         }
+
+
         return new int[0];
     }
     //-------------------------------------------------------
