f611c009b8dfe580561c9845a9b43419ab80e028
dbnaruto <dbnaruto1@gmail.com>
2021-09-18 22:48:48 -0400

    LeetCode-1 Slight optimization

--- src/main/java/com/dbnaruto/leetcode/n1_TwoSum.java
+++ src/main/java/com/dbnaruto/leetcode/n1_TwoSum.java
@@ -12,9 +12,11 @@ public class n1_TwoSum {
     //-------------------------------------------------------
     public static int[] twoSum(int[] nums, int target) {
         for(int i = 0; i < nums.length; i++) {
-            for(int j = i+1; j < nums.length; j++) {
-                if (nums[i]+nums[j] == target ) {
-                    return new int[]{i, j};
+            if (Math.abs(i) < Math.abs(target) || 0 == target) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[]{i, j};
+                    }
                 }
             }
         }
--- src/test/java/com/dbnaruto/leetcode/n1_TwoSumTest.java
+++ src/test/java/com/dbnaruto/leetcode/n1_TwoSumTest.java
@@ -1,6 +1,6 @@
 package test.java.com.dbnaruto.leetcode;
 
-import main.java.com.dbnaruto.leetcode.*;
+import main.java.com.dbnaruto.leetcode.n1_TwoSum;
 import org.junit.jupiter.api.Assertions;
 
 class n1_TwoSumTest {
@@ -39,10 +39,32 @@ class n1_TwoSumTest {
     void testContainsDuplicate_FirstWrong() {
 
         // j should have = i+1 not a hardcoded 1
-        int[] nums = {2,5,5,11};
+        int[] nums = {2, 5, 5, 11};
         int target = 10;
         int[] result = n1_TwoSum.twoSum(nums, target);
-        int[] expected = {1,2};
+        int[] expected = {1, 2};
+        Assertions.assertArrayEquals(result, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testContainsDuplicate_SecondWrong() {
+
+        // 0 is a number
+        int[] nums = {0, 4, 3, 0};
+        int target = 0;
+        int[] result = n1_TwoSum.twoSum(nums, target);
+        int[] expected = {0, 3};
+        Assertions.assertArrayEquals(result, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testContainsDuplicate_ThirdWrong() {
+
+        // If checking value is "less than" target, must use absolute value to accommodate negative ints
+        int[] nums = {-1, -2, -3, -4, -5};
+        int target = -8;
+        int[] result = n1_TwoSum.twoSum(nums, target);
+        int[] expected = {2, 4};
         Assertions.assertArrayEquals(result, expected);
     }
 }
